729c32c84a1e085e6076d41e6c14a57926d4f516
kudokaisei <kudokaisei@gmail.com>
2020-09-08 10:42:49 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tasks#index"
+  resources :users, only: [:edit, :update]
   resources :tasks,only: [:index, :create, :destroy, :edit, :update]
 end
