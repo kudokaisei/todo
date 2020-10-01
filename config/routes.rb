0a6196f03f433d2bf073fab50be2b23bc5c4200c
kudokaisei <kudokaisei@gmail.com>
2020-10-01 17:38:38 +0900

    user-group

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "tasks#index"
   resources :users, only: [:edit, :update, :show]
+  resources :groups, only: [:new, :create]
   resources :tasks
 end
