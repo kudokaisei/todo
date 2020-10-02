b850d15f8767eb8b827e260d70dc576894d18540
kudokaisei <kudokaisei@gmail.com>
2020-10-03 07:32:27 +0900

    Update 20200907032355_create_tasks.rb

--- db/migrate/20200907032355_create_tasks.rb
+++ db/migrate/20200907032355_create_tasks.rb
@@ -2,7 +2,9 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :task
-      t.string :weektask
+      t.text :detalis
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
